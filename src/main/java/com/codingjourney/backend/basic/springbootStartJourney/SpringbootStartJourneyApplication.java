717d309c4d07ea184a5010cc98ce82c3f8c4603f
Rahul Raj <raj.rahul686@gmail.com>
2024-12-07 01:58:33 +0530

    basic structure of project is implemented

---
+++ src/main/java/com/codingjourney/backend/basic/springbootStartJourney/SpringbootStartJourneyApplication.java
@@ -0,0 +1,13 @@
+package com.codingjourney.backend.basic.springbootStartJourney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootStartJourneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootStartJourneyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codingjourney/backend/basic/springbootStartJourney/SpringbootStartJourneyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingjourney.backend.basic.springbootStartJourney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootStartJourneyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
